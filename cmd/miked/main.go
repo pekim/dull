d02194c4d9927d85bf54de808a48dffdd022e90f
Mike D Pilsbury <mike.pilsbury@gmail.com>
2017-10-18 21:06:05 +0100

    Show an empty window using glfw.

--- cmd/miked/main.go
+++ cmd/miked/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
 // Build is populated with a git revision. (see Makefile)
@@ -10,6 +13,32 @@ var Build string
 // Version is populated with a version string. (see Makefile)
 var Version string
 
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
+
 func main() {
-	fmt.Println("main", Version, Build)
+	fmt.Println(Version)
+	fmt.Println(Build)
+
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.WaitEvents()
+	}
 }
