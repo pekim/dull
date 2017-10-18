3915a0feb67b4184d061dfbeddca4a378d3c11bc
Mike D Pilsbury <mike.pilsbury@gmail.com>
2017-10-18 20:18:10 +0100

    Add doc comments for main vars populated by build.

--- cmd/miked/main.go
+++ cmd/miked/main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 )
 
+// Build is populated with a git revision. (see Makefile)
 var Build string
+
+// Version is populated with a version string. (see Makefile)
 var Version string
 
 func main() {
