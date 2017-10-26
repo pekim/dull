722e8cbd54dd6b1ea700b130be025d404351956a
Mike D Pilsbury <mike.pilsbury@gmail.com>
2017-10-26 12:23:48 +0100

    Add more cell changing to demo.

--- _demo/simple/main.go
+++ _demo/simple/main.go
@@ -28,6 +28,7 @@ func initialise(app *dull.Application, err error) {
 	window.SetPosition(200, 200)
 	window.Show()
 
+	// invert a couple of cells periodically
 	go func() {
 		t := time.Tick(1 * time.Second)
 		for range t {
@@ -42,6 +43,24 @@ func initialise(app *dull.Application, err error) {
 				if err == nil {
 					cell2.Invert = !cell2.Invert
 					cell2.MarkDirty()
+				}
+			})
+		}
+	}()
+
+	// change all cells' rune periodically
+	go func() {
+		b := false
+
+		t := time.Tick(1800 * time.Millisecond)
+		for range t {
+			window.Do(func() {
+				b = !b
+
+				if b {
+					window.Cells.SetAllCellsRune(' ')
+				} else {
+					window.Cells.SetAllCellsRune('*')
 				}
 			})
 		}
