ce062b196cf0a2d017adf7a75e970c6b4c54402e
Mike D Pilsbury <mike.pilsbury@gmail.com>
2017-10-29 12:26:26 +0000

    Patch horizontal line glyphs to avoid the effects of antialiasing.
    This ensures that horizontally adjacent strikethrough and underline
    glyphs for a continuous line.

--- internal/font/fonttextureatlas.go
+++ internal/font/fonttextureatlas.go
@@ -29,9 +29,43 @@ func (fta *FontTextureAtlas) GetGlyph(rune rune) *textureatlas.TextureItem {
 		panic(err)
 	}
 
+	if rune == '\u0332' || rune == '\u0336' {
+		fta.patchHorizontalLineGlyph(rune, fontGlyph)
+	}
+
 	glyph = fta.textureAtlas.AddItem(key, fontGlyph.Bitmap, fontGlyph.BitmapWidth, fontGlyph.BitmapHeight,
 		float32(fontGlyph.TopBearing), float32(fontGlyph.LeftBearing),
 	)
 
 	return glyph
+}
+
+// patchHorizontalLineGlyph patches bitmaps for horizontal line
+// glyphs that are expected to join with the same glyph in horizontally
+// adjacent cells.
+//
+// The problem is that the left and/or right ends of the line may fade
+// a little because of antialiasing.
+//
+// To address this, for each line the value of the middle pixel is used
+// for all pixels in the row. The effect is that the ends of the lines
+// are made to have the same value as the rest of the line.
+func (fta *FontTextureAtlas) patchHorizontalLineGlyph(char rune, glyph *Glyph) {
+	if glyph.BitmapWidth < 3 || glyph.BitmapHeight < 1 {
+		return
+	}
+
+	bitmap := *glyph.Bitmap
+	width := glyph.BitmapWidth
+	height := glyph.BitmapHeight
+	middleOfRowOffset := width / 2
+
+	for row := 0; row < height; row++ {
+		rowStart := row * width
+		middlePixelValue := bitmap[rowStart+middleOfRowOffset]
+
+		for column := 0; column < width; column++ {
+			bitmap[rowStart+column] = middlePixelValue
+		}
+	}
 }
