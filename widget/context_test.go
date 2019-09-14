629ca346e34a1ec43a054a7725c5b15eb50bbaed
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-09-14 15:23:00 +0100

    Simplifiy focus next test.

--- widget/context_test.go
+++ widget/context_test.go
@@ -5,10 +5,25 @@ import (
 	"testing"
 )
 
+/*
+	Use nested children.
+
+	child1				Text
+	child2				Label; not focusable
+	child 3				Flex
+		child 31		Text
+		child 32		Text
+*/
 func TestContext_SetNextFocusableWidget(t *testing.T) {
 	child1 := NewText("child 1", nil)
+
 	child2 := NewLabel("child 2", nil)
-	child3 := NewText("child 2", nil)
+
+	child3 := NewFlex(DirectionHorizontal)
+	child31 := NewText("child 3-1", nil)
+	child32 := NewText("child 3-2", nil)
+	child3.Add(child31, FlexChildOptions{})
+	child3.Add(child32, FlexChildOptions{})
 
 	root := NewFlex(DirectionHorizontal)
 	root.Add(child1, FlexChildOptions{})
@@ -27,41 +42,10 @@ func TestContext_SetNextFocusableWidget(t *testing.T) {
 	assert.Equal(t, child1.Text(), context.focusedWidget.(*Text).Text())
 
 	context.SetNextFocusableWidget()
-	assert.Equal(t, child3.Text(), context.focusedWidget.(*Text).Text())
+	assert.Equal(t, child31.Text(), context.focusedWidget.(*Text).Text())
 
 	context.SetNextFocusableWidget()
-	assert.Equal(t, child1.Text(), context.focusedWidget.(*Text).Text())
-}
-
-func TestContext_SetNextFocusableWidget_NestedWidgets(t *testing.T) {
-	child1 := NewText("child 1", nil)
-
-	child2 := NewFlex(DirectionHorizontal)
-	child21 := NewText("child 2-1", nil)
-	child22 := NewText("child 2-2", nil)
-	child2.Add(child21, FlexChildOptions{})
-	child2.Add(child22, FlexChildOptions{})
-
-	root := NewFlex(DirectionHorizontal)
-	root.Add(child1, FlexChildOptions{})
-	root.Add(child2, FlexChildOptions{})
-
-	context := &Context{
-		root: &Root{
-			child: root,
-		},
-		focusedWidget: nil,
-	}
-	context.ensureFocusedWidget()
-
-	context.ensureFocusedWidget()
-	assert.Equal(t, child1.Text(), context.focusedWidget.(*Text).Text())
-
-	context.SetNextFocusableWidget()
-	assert.Equal(t, child21.Text(), context.focusedWidget.(*Text).Text())
-
-	context.SetNextFocusableWidget()
-	assert.Equal(t, child22.Text(), context.focusedWidget.(*Text).Text())
+	assert.Equal(t, child32.Text(), context.focusedWidget.(*Text).Text())
 
 	context.SetNextFocusableWidget()
 	assert.Equal(t, child1.Text(), context.focusedWidget.(*Text).Text())
