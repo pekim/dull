2da5da37228096758f942b9c94c467e758dadc07
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-09-14 11:59:56 +0100

    Add test for Context.SetNextFocusableWidget().

--- widget/context_test.go
+++ widget/context_test.go
@@ -1,9 +1,32 @@
 package widget
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestContext_SetNextFocusableWidget(t *testing.T) {
-	//assert.Fail(t, "TODO")
+	child1 := NewText("1", nil)
+	child2 := NewText("1", nil)
+
+	root := NewFlex(DirectionHorizontal)
+	root.Add(child1, FlexChildOptions{})
+	root.Add(child2, FlexChildOptions{})
+
+	context := &Context{
+		root: &Root{
+			child: root,
+		},
+		focusedWidget: nil,
+	}
+	context.ensureFocusedWidget()
+
+	context.ensureFocusedWidget()
+	assert.Equal(t, child1, context.focusedWidget)
+
+	context.SetNextFocusableWidget()
+	assert.Equal(t, child2, context.focusedWidget)
+
+	context.SetNextFocusableWidget()
+	assert.Equal(t, child1, context.focusedWidget)
 }
