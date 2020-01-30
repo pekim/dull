f7e6dd5ea6aed08dec71d499c9c14318f157e35d
Mike D Pilsbury <mike.pilsbury@gmail.com>
2020-01-30 17:55:37 +0000

    Create a crude, untested button widget.

---
+++ imui/widget/button.go
@@ -0,0 +1,25 @@
+package widget
+
+import (
+	"github.com/pekim/dull"
+	"github.com/pekim/dull/imui"
+)
+
+func Button(r imui.Renderer, label string, x, y float32) {
+	d := r.Drawer()
+	fg := dull.NewColor(0.5, 0.5, 0.5, 1.0)
+	bg := dull.NewColor(0.0, 0.0, 0.0, 0.0) // transparent
+
+	if r.IsFocused() {
+		bg = dull.NewColor(1.0, 0.0, 0.0, 0.8) // red
+	}
+
+	for i, ch := range label {
+		cell := &dull.Cell{
+			Rune: ch,
+			Fg:   fg,
+			Bg:   bg,
+		}
+		d.DrawCell(cell, x+float32(i), y)
+	}
+}
