96de39b16bd9ee6b5eb4da454e9a78c6cc4c13e2
Mike D Pilsbury <mike.pilsbury@gmail.com>
2020-02-01 10:11:09 +0000

    Fix visual regression test.

--- window_test.go
+++ window_test.go
@@ -5,10 +5,20 @@ package dull
 import "testing"
 
 func TestWindowSimple(t *testing.T) {
-	testCaptureAndCompareImage(t, "simple",
-		200, 200, 2.0,
+	testCaptureAndCompareImage(t, "simple", 200, 200, 2.0,
+
 		func(window *Window) {
-			window.Grid().PrintAt(0, 1, "Qaz")
+			window.SetDrawCallback(func(drawer Drawer, columns, rows int) {
+				for i, r := range "Qaz" {
+					drawer.DrawCell(&Cell{
+						Rune: r,
+						Fg:   Color{0.0, 0.0, 0.0, 1.0},
+						Bg:   Color{},
+					}, float32(i), 1)
+				}
+			})
+
+			window.Draw()
 		},
 	)
 }
