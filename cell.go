ac8e9bc508d6b47a9f4702ea05f62e2efe9000b1
Mike D Pilsbury <mike.pilsbury@gmail.com>
2020-06-13 12:16:43 +0100

    Remove misleading comments and unused function.

--- cell.go
+++ cell.go
@@ -13,11 +13,6 @@ type CellOptions struct {
 }
 
 // Cell represents a single cell in a grid of Cells, that are displayed in a window.
-//
-// Fields in a Cell may be modified in a callback that runs on the main thread.
-// Do not modify the cells outside of a mainthread callback.
-//
-// If any fields are modified, then the containing window's MarkDirty must be called.
 type Cell struct {
 	// Rune is the Rune to be rendered.
 	Rune rune
@@ -40,14 +35,4 @@ type Cell struct {
 
 	// Invert denotes whether the foreground and background colours should be reversed.
 	Invert bool
-}
-
-func (c *Cell) ApplyOptions(options *CellOptions) {
-	c.Fg = options.Fg
-	c.Bg = options.Bg
-	c.Bold = options.Bold
-	c.Invert = options.Invert
-	c.Italic = options.Italic
-	c.Strikethrough = options.Strikethrough
-	c.Underline = options.Underline
 }
