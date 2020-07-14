e53776045509bed28632924c13d10607c099f15b
Mike D Pilsbury <mike.pilsbury@gmail.com>
2020-07-14 17:05:25 +0100

    Use flex layout for label-wrap demo.

--- _demo/label-wrap/main.go
+++ _demo/label-wrap/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/pekim/dull"
 	"github.com/pekim/dull/color"
-	"github.com/pekim/dull/geometry"
 	"github.com/pekim/dull/ui"
+	"github.com/pekim/dull/ui/layout"
 	"github.com/pekim/dull/ui/widget"
 )
 
@@ -16,8 +16,8 @@ func initialise(app *dull.Application, err error) {
 	window, err := app.NewWindow(&dull.WindowOptions{
 		Bg:     &color.White,
 		Fg:     &color.Black,
-		Width:  1000,
-		Height: 700,
+		Width:  800,
+		Height: 800,
 	})
 	if err != nil {
 		panic(err)
@@ -29,17 +29,26 @@ func initialise(app *dull.Application, err error) {
 		column int
 	}
 
-	backgrounds := []color.Color{color.Blue1, color.Lightgray, color.Brown}
-	foregrounds := []color.Color{color.White, color.Black, color.White}
-
-	labels := []label{}
+	backgroundsOddRow := []color.Color{color.Blue1, color.Lightgray, color.Brown}
+	foregroundsOddRow := []color.Color{color.White, color.Black, color.White}
+	backgroundsEvenRow := []color.Color{color.Lightgray, color.Brown, color.Blue1}
+	foregroundsEvenRow := []color.Color{color.Black, color.White, color.White}
 
 	addLabel := func(
+		layout *layout.Flex,
+		row int,
+		column int,
 		text string,
 		halign ui.HAlign,
 		valign ui.VAlign,
-		row int, column int,
 	) {
+		backgrounds := backgroundsEvenRow
+		foregrounds := foregroundsEvenRow
+		if row%2 == 1 {
+			backgrounds = backgroundsOddRow
+			foregrounds = foregroundsOddRow
+		}
+
 		l := widget.NewLabel(text)
 		l.SetHAlign(halign)
 		l.SetVAlign(valign)
@@ -47,56 +56,42 @@ func initialise(app *dull.Application, err error) {
 		l.SetColor(foregrounds[column])
 		l.SetWrap(true)
 
-		labels = append(labels, label{
-			label:  l,
-			row:    row,
-			column: column,
-		})
+		style := layout.InsertWidget(l, column)
+		style.SetGrow(1)
 	}
-
-	row := 0
 
 	someLongeText := "some longer text, that will wrap across lines"
 
-	addLabel("left:top "+someLongeText, ui.HAlignLeft, ui.VAlignTop, row, 0)
-	addLabel("centre:top "+someLongeText, ui.HAlignCentre, ui.VAlignTop, row, 1)
-	addLabel("right:top "+someLongeText, ui.HAlignRight, ui.VAlignTop, row, 2)
+	column := layout.NewFlex(layout.FlexDirectionColumn)
+	row := 0
+
+	row1 := layout.NewFlex(layout.FlexDirectionRow)
+	addLabel(row1, row, 0, "left:top "+someLongeText, ui.HAlignLeft, ui.VAlignTop)
+	addLabel(row1, row, 1, "centre:top "+someLongeText, ui.HAlignCentre, ui.VAlignTop)
+	addLabel(row1, row, 2, "right:top "+someLongeText, ui.HAlignRight, ui.VAlignTop)
+	row1Style := column.InsertWidget(row1, row)
+	row1Style.SetGrow(1)
 	row++
 
-	addLabel("clipped ----------------- "+someLongeText, ui.HAlignLeft, ui.VAlignTop, row, 0)
-	addLabel("-------- clipped -------- "+someLongeText, ui.HAlignCentre, ui.VAlignTop, row, 1)
-	addLabel("----------------- clipped "+someLongeText, ui.HAlignRight, ui.VAlignTop, row, 2)
+	row2 := layout.NewFlex(layout.FlexDirectionRow)
+	addLabel(row2, row, 0, "left:centre "+someLongeText, ui.HAlignLeft, ui.VAlignCentre)
+	addLabel(row2, row, 1, "centre:centre "+someLongeText, ui.HAlignCentre, ui.VAlignCentre)
+	addLabel(row2, row, 2, "right:centre "+someLongeText, ui.HAlignRight, ui.VAlignCentre)
+	row2Style := column.InsertWidget(row2, row)
+	row2Style.SetGrow(1)
 	row++
 
-	addLabel("left:centre "+someLongeText, ui.HAlignLeft, ui.VAlignCentre, row, 0)
-	addLabel("centre:centre "+someLongeText, ui.HAlignCentre, ui.VAlignCentre, row, 1)
-	addLabel("right:centre "+someLongeText, ui.HAlignRight, ui.VAlignCentre, row, 2)
-	row++
-
-	addLabel("left:bottom "+someLongeText, ui.HAlignLeft, ui.VAlignBottom, row, 0)
-	addLabel("centre:bottom "+someLongeText, ui.HAlignCentre, ui.VAlignBottom, row, 1)
-	addLabel("right:bottom "+someLongeText, ui.HAlignRight, ui.VAlignBottom, row, 2)
+	row3 := layout.NewFlex(layout.FlexDirectionRow)
+	addLabel(row3, row, 0, "left:bottom "+someLongeText, ui.HAlignLeft, ui.VAlignBottom)
+	addLabel(row3, row, 1, "centre:bottom "+someLongeText, ui.HAlignCentre, ui.VAlignBottom)
+	addLabel(row3, row, 2, "right:bottom "+someLongeText, ui.HAlignRight, ui.VAlignBottom)
+	row3Style := column.InsertWidget(row3, row)
+	row3Style.SetGrow(1)
 	row++
 
 	window.SetDrawCallback(func(d dull.Drawer, columns, rows int) {
 		vp := dull.ViewportForWindow(window, d)
-
-		itemWidth := 30
-		itemHeight := 6
-		columnGap := 2
-		rowGap := 1
-
-		for _, label := range labels {
-			top := label.row * (itemHeight + rowGap)
-			left := label.column * (itemWidth + columnGap)
-
-			label.label.Draw(vp.View(geometry.RectFloat{
-				Top:    float64(top),
-				Bottom: float64(top + itemHeight),
-				Left:   float64(left),
-				Right:  float64(left + itemWidth),
-			}))
-		}
+		column.Draw(vp)
 	})
 
 	window.Show()
